3a27eb73b4e35762b46feb86449ffcf0b720e210
garciadamian <damiansantiago142@gmail.com>
2025-09-20 09:08:01 -0600

    operaciones basicas

---
+++ src/app/formularios/cinepolis/cinepolis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis',
+  standalone: false,
+  templateUrl: './cinepolis.component.html',
+  styleUrl: './cinepolis.component.css'
+})
+export class CinepolisComponent {
+
+}
---
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  standalone: false,
+  templateUrl: './operas-bas.component.html',
+  styleUrl: './operas-bas.component.css'
+})
+export class OperasBasComponent {
+
+  num1:string="";
+  num2:string="";
+  res:string="";
+  operacio:string="";
+
+
+
+operaci():void{
+  if (this.operacio){
+  switch(this.operacio){
+
+  case 'suma':
+    this.res=(parseInt(this.num1)+parseInt(this.num2)).toString();
+  break;
+
+  case 'rest':
+    this.res=(parseInt(this.num1)-parseInt(this.num2)).toString();
+  break;
+
+  case 'multi':
+    this.res=(parseInt(this.num1)*parseInt(this.num2)).toString();
+  break;
+  case 'divi':
+    this.res=(parseInt(this.num1)/parseInt(this.num2)).toString();
+  break;
+  }
+    }
+  }
+}
